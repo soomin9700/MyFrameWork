d9736ced3ddf9fe6d2714388c4c777a66a111277
MiantsaSarindra <soominsoomin65@gmail.com>
2026-07-17 06:11:41 +0300

    miantso anaty frontController

--- src/main/java/com/framework/servlet/FrontControllerServlet.java
+++ src/main/java/com/framework/servlet/FrontControllerServlet.java
@@ -3,10 +3,13 @@ package com.framework.servlet;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import com.framework.annotation.Controller;
 import com.framework.core.AnnotationChecker;
 import com.framework.core.PackageScanner;
+import com.framework.core.RouteHandler;
+import com.framework.core.RouteResolver;
 
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
@@ -17,6 +20,7 @@ import jakarta.servlet.http.HttpServletResponse;
 public class FrontControllerServlet extends HttpServlet {
 
     private List<String> listController = new ArrayList<>();
+    private Map<String, RouteHandler> routeTable;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
@@ -33,16 +37,19 @@ public class FrontControllerServlet extends HttpServlet {
                 }
             }
 
+            routeTable = RouteResolver.resoudre(packageControllers);
+
             System.out.println("=== Initialisation du FrontController ===");
             System.out.println("Package scanne : " + packageControllers);
             System.out.println("Controllers trouves : " + listController.size());
+            System.out.println("Routes trouvees : " + routeTable.size());
 
-            for (String nomController : listController) {
-                System.out.println(" -> " + nomController);
+            for (Map.Entry<String, RouteHandler> entree : routeTable.entrySet()) {
+                System.out.println(" -> " + entree.getKey() + " => " + entree.getValue());
             }
 
         } catch (Exception e) {
-            throw new ServletException("Impossible d'initialiser la liste des controllers", e);
+            throw new ServletException("Impossible d'initialiser le FrontController", e);
         }
     }
 
@@ -63,14 +70,21 @@ public class FrontControllerServlet extends HttpServlet {
 
         response.setContentType("text/plain");
 
-        String uri = request.getRequestURI();
+        String url = request.getRequestURI().substring(request.getContextPath().length());
 
-        response.getWriter().println("FrontController OK");
-        response.getWriter().println("URL appelée : " + uri);
-        response.getWriter().println("Nombre de controllers detectes : " + listController.size());
+        RouteHandler handler = routeTable.get(url);
 
-        for (String nomController : listController) {
-            response.getWriter().println(" - " + nomController);
+        if (handler == null) {
+            response.getWriter().println("Aucune route ne correspond a : " + url);
+            response.getWriter().println("Routes disponibles :");
+            for (String routeDisponible : routeTable.keySet()) {
+                response.getWriter().println(" - " + routeDisponible);
+            }
+            return;
         }
+
+        response.getWriter().println("URL demandee   : " + url);
+        response.getWriter().println("Controller     : " + handler.getControllerClassName());
+        response.getWriter().println("Methode        : " + handler.getMethodName());
     }
 }
